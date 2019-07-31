dcde30c7224b69a64fd3ca419cb2c87ffa4a387d
Graham Fairweather <xotic750@gmail.com>
2019-07-31 17:40:24 +0200

    Package rework

--- dist/trim-right-x.js
+++ dist/trim-right-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-29T21:43:41.480Z",
+  "date": "2019-07-31T15:40:16.768Z",
   "describe": "",
   "description": "This method removes whitespace from the end of a string.",
   "file": "trim-right-x.js",
